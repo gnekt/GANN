088275fa2888a7d487679934b194cdc22586615b
Christian Di Maio <39567665+gnekt@users.noreply.github.com>
2021-01-25 03:41:17 +0100

    Update

--- GeneralANN/NeuralNetwork.py
+++ GeneralANN/NeuralNetwork.py
@@ -8,7 +8,8 @@ from random import random
 from GeneralANN.Neuron import Neuron
 from random import *
 from enum import Enum
-import  numpy as np
+import numpy as np
+from keras import *
 from ann_visualizer.visualize import ann_viz
 
 class NeuralNetworkScope(Enum):
@@ -75,26 +76,50 @@ class NeuralNetwork:
         for i in reversed(range(len(self.network))):
             layer = self.network[i]
             errors = list()
-            if i != len(self.network) - 1:
-                for j in range(len(layer)):
+            if i != len(self.network) - 1: #Calculate error on the hidden layers
+                for neuron_idx in range(len(layer)):
                     error = 0.0
                     for neuron in self.network[i + 1]:
-                        error += (neuron.get_weights()[j] * neuron.get_delta())
+                        # I assumed that the neurons of the layer above have already calculated their own deltas
+                        #                     # and it works bcz the reversed loop :)
+                        error += (neuron.get_weights()[neuron_idx] * neuron.get_delta())
                     errors.append(error)
             else:
-                for j in range(len(layer)):
+                for j in range(len(layer)): #Calculate error on the output layer
                     neuron = layer[j]
                     errors.append(expected[j] - neuron.get_output())
             for j in range(len(layer)):
                 neuron = layer[j]
                 neuron.set_delta(errors[j] * neuron.response_derivative())
 
+    # def backward_propagate(self,expected):
+    #     for layer_idx in reversed(range(len(self.network))):
+    #         layer = self.network[layer_idx]
+    #         errors = list()
+    #         if layer_idx != len(self.network) - 1: # Calculate error on the hidden layers
+    #             for neuron_idx in range(len(layer)): # get the index of j-th neuron of this layer and iterate on it
+    #                 error = 0.0
+    #                 for neuron_above in self.network[layer_idx + 1]: # Get all neurons above
+    #                     # I assumed that the neurons of the layer above have already calculated their own deltas
+    #                     # and it works bcz the reversed loop :)
+    #                     error += (neuron_above.get_weights()[neuron_idx] * neuron.get_delta())
+    #                 errors.append(error)
+    #         else: # Calculate error on the output layer
+    #             for neuron_idx in range(len(layer)):
+    #                 neuron = layer[neuron_idx]
+    #                 errors.append(expected[neuron_idx] - neuron.get_output())
+    #         for neuron_idx in range(len(layer)):
+    #             neuron = layer[neuron_idx] # Pick a neuron of this layer
+    #             # Indipendently if the layer is hidden or its the output i just calculate the delta as
+    #             # error * derivative of the response
+    #             neuron.set_delta(errors[neuron_idx] * neuron.response_derivative())
+
     def update_weights(self, inputs, l_rate): # Stochastic Gradient Descent
-        for i in range(len(self.network)):
+        for layer_idx in range(len(self.network)):
             inputs = inputs[:-1]
-            if i != 0:
-                inputs = [neuron.get_output() for neuron in self.network[i - 1]]
-            for neuron in self.network[i]:
+            if layer_idx != 0:
+                inputs = [neuron.get_output() for neuron in self.network[layer_idx - 1]]
+            for neuron in self.network[layer_idx]:
                 for j in range(len(inputs)):
                     neuron.get_weights()[j] += l_rate * neuron.get_delta() * inputs[j]
                 neuron.get_weights()[-1] += l_rate * neuron.get_delta()
@@ -105,16 +130,21 @@ class NeuralNetwork:
         if self.Scope == NeuralNetworkScope.Regression:
             raise Exception("Hey man, you defined this network to work with regression problem, pay attention! :D")
         # Normalize the input
+        Input = train[:,:-1]
+        Normalized_input = Input / Input.max()
+        Target = train[:,-1:]
         for epoch in range(n_epoch):
             sum_error = 0
-            for row in train:
-                outputs = self.propagate(row)
+            for inputs,target in zip(Normalized_input,Target):
+                outputs = self.propagate(inputs)
                 # Apply the one hot encoding into 2 line of code, thanks python :)
-                expected = [0 for i in range(n_outputs)]
-                expected[row[-1] if type(row[-1]) is int else row[-1].value] = 1
+                if n_outputs != 1:
+                    expected = [0 for i in range(n_outputs)]
+                    expected[np.asscalar(target) if type(np.asscalar(target)) is int else np.asscalar(target).value] = 1
+                expected = target
                 sum_error += sum([(expected[i] - outputs[i]) ** 2 for i in range(len(expected))]) / len(train)
                 self.backward_propagate(expected)
-                self.update_weights(row, l_rate)
+                self.update_weights(inputs, l_rate)
             print(f'>epoch={epoch:d}, lrate={l_rate:.3f}, error={math.sqrt(sum_error):.3f},'
                   f'     accuracy={(1-math.sqrt(sum_error))*100:.2f}%')
 
@@ -137,24 +167,27 @@ class NeuralNetwork:
 
     def predict(self, row):
         outputs = self.propagate(row)
+        # Assume that in the classificator each output neuron is ordered from left to right i pick up the index of the
+        # neuron that respond with the maximum value :)
         return outputs.index(max(outputs)) if self.Scope == NeuralNetworkScope.Classification else max(outputs)
 
-    # def show_me(self):
-    #     _network = models.Sequential()  # Output of each layer we add, is the input to the next layer we specify
-    #     #1st Hidden Layer#1
-    #     _network.add(layers.Dense(units=self.N_inputs,  # Dense stay for a fully connected layer
-    #                              activation='sigmoid',
-    #                              kernel_initializer='uniform',
-    #                              input_dim=self.N_inputs))
-    #     #Deep arch.
-    #     for i in range(self.N_hidden_layers -1):
-    #         _network.add(layers.Dense(units=self.N_neuron_for_hidden_layer,
-    #                                  activation='sigmoid',
-    #                                  kernel_initializer='uniform'))
-    #     #Output layer
-    #     _network.add(layers.Dense(units=self.N_outputs,
-    #                              activation='sigmoid',
-    #                              kernel_initializer='uniform'))
-    #     #Visualize it
-    #     ann_viz(_network, title="My ANN")
+
+    def show_me(self):
+        _network = models.Sequential()  # Output of each layer we add, is the input to the next layer we specify
+        #1st Hidden Layer#1
+        _network.add(layers.Dense(units=self.N_neuron_for_hidden_layer,  # Dense stay for a fully connected layer
+                                 activation='sigmoid',
+                                 kernel_initializer='uniform',
+                                 input_dim=self.N_inputs))
+        #Deep arch.
+        for i in range(self.N_hidden_layers -1):
+            _network.add(layers.Dense(units=self.N_neuron_for_hidden_layer,
+                                     activation='sigmoid',
+                                     kernel_initializer='uniform'))
+        #Output layer
+        _network.add(layers.Dense(units=self.N_outputs,
+                                 activation='sigmoid',
+                                 kernel_initializer='uniform'))
+        #Visualize it
+        ann_viz(_network, title="My ANN")
     #
